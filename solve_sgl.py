86bea66938381ec8aff0bb83fde035dd12859d78
Mirco <mirco-hellwig@web.de>
2018-09-08 19:40:14 +0200

    Added directory as command line argument and made solve_sgl.py executable

--- solve_sgl.py
+++ solve_sgl.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
+import argparse
 import iomodul
 import sgl
 
 
 
+
 def main():
-    para=iomodul.read_schrodinger_inp()
-    eigenvalues, eigenvectors=sgl.solve_hamiltonian(para)
-    sgl.expectation_values(para,eigenvectors)
+    parser = argparse.ArgumentParser(description='Solving 1-d SGL')
+    msg = 'Directory (default: .)'
+    parser.add_argument('-d', '--directory', default='.', help=msg)
+    args = parser.parse_args()
+    para = iomodul.read_schrodinger_inp(args.directory)
+    eigenvalues, eigenvectors = sgl.solve_hamiltonian(para)
+    sgl.expectation_values(para, eigenvectors)
+
 if __name__ == '__main__':
     main()
     
