ad2172e91b6c8ea596fd3f82ef4602e57b741bdd
Mirco <mirco-hellwig@web.de>
2018-09-09 18:18:55 +0200

    Added command line parser

--- visualizer.py
+++ visualizer.py
@@ -6,8 +6,19 @@ Created on Sun Sep  9 12:51:42 2018
 @author: mirco
 """
 
+import argparse
 import plotmodul
 import matplotlib.pyplot as plt
 
-plotmodul.plot_sgl_solution('./iodata/infinite_well_pot/', scaling=0.3)
-plt.show()
+def main():
+    parser = argparse.ArgumentParser(description='Plotting solution of 1-d SGL')
+    msg = 'Directory (default: .)'
+    parser.add_argument('-d', '--directory', default='.', help=msg)
+    msg = 'Scaling of eigenvectors (default: 1.0)'
+    parser.add_argument('-s', '--scaling', default=1.0, help=msg)
+    args = parser.parse_args()
+    plotmodul.plot_sgl_solution(args.directory, float(args.scaling))
+    plt.show()
+
+if __name__ == '__main__':
+    main()
