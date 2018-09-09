a95fdb350b934f1d741147f813f189886d8b8769
Mirco <mirco-hellwig@web.de>
2018-09-09 11:41:52 +0200

    Added writing of eigenvalues, eigenvectors not working

--- sgl.py
+++ sgl.py
@@ -6,19 +6,16 @@ import scipy.linalg
 import iomodul
 
 def _interpolate_potential(para):
+    xaxis = np.linspace(para['xMin'], para['xMax'], para['nPoints'])
     if para['interpolation_type']=='linear':
         interpolation_fun = scipy.interpolate.interp1d(para['interpolation_points_x'], para['interpolation_points_y'])
-        xaxis=np.linspace(para['xMin'],para['xMax'],para['nPoints'])
         potential=interpolation_fun(xaxis)
     elif para['interpolation_type']=='cspline':
         interpolation_fun = scipy.interpolate.CubicSpline(para['interpolation_points_x'], para['interpolation_points_y'])
-        xaxis=np.linspace(para['xMin'],para['xMax'],para['nPoints'])
         potential=interpolation_fun(xaxis)
     elif para['interpolation_type']=='polynomial':
         print('not yet implemented')
-    xpot = np.linspace(para['xMin'], para['xMax'], para['nPoints'])
-    iomodul.write_pot_data(xpot, potential,
-                           './iodata/finite_well_pot/potential.dat')
+    iomodul.write_potential(xaxis, potential, para)
     return potential
 
 def _write_hamiltonian(para):
@@ -32,4 +29,7 @@ def _write_hamiltonian(para):
 def solve_hamiltonian(para):
     [hamiltonian_diag, hamiltonian_offdiag] =_write_hamiltonian(para)
     eigenvalues, eigenvectors=scipy.linalg.eigh_tridiagonal(hamiltonian_diag, hamiltonian_offdiag,False,'v',(para['first_eigenvalue'],para['last_eigenvalue']))
+    iomodul.write_eigenvalues(eigenvalues, para)
+    xaxis = np.linspace(para['xMin'], para['xMax'], para['nPoints'])
+    iomodul.write_eigenvectors(eigenvectors, xaxis, para)
     return eigenvalues, eigenvectors
